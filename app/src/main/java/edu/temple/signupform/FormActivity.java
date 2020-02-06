3905744010676c61544f33fa7a81bbdaf896d12f
Dan Tran <tug06533@temple.edu>
2020-02-06 16:38:24 -0500

    added default on click listener and changed ID values

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -3,6 +3,8 @@ package edu.temple.signupform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class FormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText userName = findViewById(R.id.usernameField);
+        EditText email = findViewById(R.id.emailField);
+        EditText pass = findViewById(R.id.passwordField);
+        EditText passConf = findViewById(R.id.passwordConfirmField);
+
+        findViewById(R.id.saveButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
+
     }
 }
